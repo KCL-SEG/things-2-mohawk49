5a56a3d7270a2384cc4079f51fb412eee605367b
Michael Seiranian <99744831+mohawk49@users.noreply.github.com>
2022-12-06 15:54:33 +0000

    Update admin.py

--- things/admin.py
+++ things/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Thing
 
-# Register your models here.
+@admin.register(Thing)
+class ThingAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for things"""
+
+    list_display = ['name', 'description', 'quantity']
