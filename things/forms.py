4f3dde1bdfb7b9800809bd544cb7fe8dd4f66d7b
Michael Seiranian <99744831+mohawk49@users.noreply.github.com>
2022-12-06 15:54:17 +0000

    Update forms.py

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,16 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(
+        max_length=120,
+        widget=forms.Textarea
+    )
+    quantity = forms.NumberInput(attrs={'min': 0, 'max': 50})
